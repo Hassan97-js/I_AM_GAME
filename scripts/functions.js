9e278880a0a3beff1519b20bf5cd99a4164efae8
Hassan <54324376+Hassan97-js@users.noreply.github.com>
2020-10-26 13:44:47 +0100

    Improvments

--- scripts/functions.js
+++ scripts/functions.js
@@ -4,27 +4,26 @@
 const monsterHealthBar = document.querySelector("#monster-health");
 const playerHealthBar = document.querySelector("#player-health");
 const bonusLifeEl = document.querySelector("#bonus-life");
-
 const attackBtn = document.querySelector("#attack-btn");
 const strongAttackBtn = document.querySelector("#strong-attack-btn");
 const healBtn = document.querySelector("#heal-btn");
 const logBtn = document.querySelector("#log-btn");
 
-/* dealMonsterDamage function for adding random numbers to the monster's progress bar value */
+/* dealMonsterDamage function to subtract random numbers to the monster's progress bar value */
 function dealMonsterDamage(damage) {
   const dealtDamage = Math.floor(Math.random() * damage);
   monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
   return dealtDamage;
 }
 
-/* dealPlayerDamage function for adding random numbers to the player's progress bar value */
+/* dealPlayerDamage function to subtract random numbers to the player's progress bar value */
 function dealPlayerDamage(damage) {
   const dealtDamage = Math.floor(Math.random() * damage);
   playerHealthBar.value = +playerHealthBar.value - dealtDamage;
   return dealtDamage;
 }
 
-/* increasePlayerHealth function for adding the healValue the player's progress bar */
+/* increasePlayerHealth function to add the healValue the player's progress bar */
 function increasePlayerHealth(healValue) {
   playerHealthBar.value = +playerHealthBar.value + healValue;
 }
